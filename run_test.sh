dc09b8c5a962d4b8b69f4791837632a298a198da
Stéfano Stypulkowski Zanata <stefano.zanata@meltwater.com>
2020-08-31 11:46:26 -0300

    Adding lint command to test script

--- run_test.sh
+++ run_test.sh
@@ -8,7 +8,7 @@ docker run --rm -it \
   -e NODE_ENV=development \
   -v `pwd`:/app/ \
   -w /app/ \
-    node:14.9.0-alpine /bin/sh -c "npm install && npm run test"
+    node:14.9.0-alpine /bin/sh -c "npm install && npm run lint && npm run test"
 
 printf "\n\e[34;1m[Testing node 12]\e[0m\n"
 docker run --rm -it \
